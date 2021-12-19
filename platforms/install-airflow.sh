c31f359c151d74bacdc7daa800083bdaea034b96
jpoley <jason.poley@gmail.com>
2021-12-18 21:57:10 -0500

    airflow on k8s - pv

--- platforms/install-airflow.sh
+++ platforms/install-airflow.sh
@@ -1,3 +1,11 @@
+mkdir -p /opt/k8s/mount1
+mkdir -p /opt/k8s/mount2
+mkdir -p /opt/k8s/mount3
+kubectl create -f pv1.yaml
+kubectl create -f pv2.yaml
+kubectl create -f pv3.yaml
+
+
 kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm install airflow apache-airflow/airflow --namespace airflow
