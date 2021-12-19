1a3a17d7420dce8fa5fa7543ab4d16ae6b9a0182
jpoley <jason.poley@gmail.com>
2021-12-19 15:03:55 -0500

    pvc fixes maybe

--- platforms/install-airflow.sh
+++ platforms/install-airflow.sh
@@ -1,11 +1,12 @@
-sudo mkdir -p /opt/k8s/mount1
-sudo mkdir -p /opt/k8s/mount2
-sudo mkdir -p /opt/k8s/mount3
+sudo mkdir -p /opt/k8s/default
+sudo mkdir -p /opt/k8s/data
+sudo mkdir -p /opt/k8s/redis-db
+
 kubectl create -f /home/vagrant/platforms/pv1.yaml
 kubectl create -f /home/vagrant/platforms/pv2.yaml
 kubectl create -f /home/vagrant/platforms/pv3.yaml
 
+helm repo add apache-airflow https://airflow.apache.org
 
 kubectl create namespace airflow
-helm repo add apache-airflow https://airflow.apache.org
-helm install --debug airflow apache-airflow/airflow --namespace airflow
+helm install --debug airflow --set logs.persistence.enabled=true  --set logs.persistence.existingClaim=default apache-airflow/airflow --namespace airflow 
