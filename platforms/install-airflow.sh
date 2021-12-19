0b94fe27149506424ac9d6a3b2987a78236981bf
jpoley <jason.poley@gmail.com>
2021-12-18 22:38:09 -0500

    clean up

--- platforms/install-airflow.sh
+++ platforms/install-airflow.sh
@@ -1,9 +1,9 @@
 sudo mkdir -p /opt/k8s/mount1
 sudo mkdir -p /opt/k8s/mount2
 sudo mkdir -p /opt/k8s/mount3
-kubectl create -f pv1.yaml
-kubectl create -f pv2.yaml
-kubectl create -f pv3.yaml
+kubectl create -f /home/vagrant/platforms/pv1.yaml
+kubectl create -f /home/vagrant/platforms/pv2.yaml
+kubectl create -f /home/vagrant/platforms/pv3.yaml
 
 
 kubectl create namespace airflow
