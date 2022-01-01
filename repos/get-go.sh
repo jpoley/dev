167d5811b24eaa11c10ca681dbb9a70e59914c61
jpoley <jason.poley@gmail.com>
2021-12-31 20:12:33 -0500

    3

--- repos/get-go.sh
+++ repos/get-go.sh
@@ -6,4 +6,5 @@ git clone https://github.com/kubernetes/kubernetes.git
 git clone https://github.com/rclone/rclone.git
 git clone https://github.com/helm/helm.git
 git clone https://github.com/awslabs/goformation.git
+git clone https://github.com/web3coach/the-blockchain-bar.git
 git clone https://github.com/GoogleCloudPlatform/golang-samples.git
