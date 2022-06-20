1dc1e3316f469251f380bb96907e8e5d10ed3e0b
jpoley <jason.poley@gmail.com>
2022-06-19 23:25:35 -0400

    crossplane cli

--- platforms/install-crossplane.sh
+++ platforms/install-crossplane.sh
@@ -2,4 +2,7 @@ kubectl create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
-curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh | sh
+curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh > cpcli.sh
+chmod +x cpcli.sh
+./cpcli.sh
+sudo mv kubectl-crossplane /usr/bin
