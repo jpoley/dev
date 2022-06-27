d285f521f958995e64d5c82d5bcd1603c9da0908
jpoley <jason.poley@gmail.com>
2022-06-27 11:36:16 -0400

    newer go

--- installers/install-go.sh
+++ installers/install-go.sh
@@ -1,7 +1,7 @@
 echo "Installing Go"
 
-wget -q https://go.dev/dl/go1.17.3.linux-amd64.tar.gz 
-tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
+wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz 
+tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
 echo 'PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
 cd /home/vagrant
-rm -rf go1.17.3.linux-amd64.tar.gz 
+rm -rf go1.18.3.linux-amd64.tar.gz 
