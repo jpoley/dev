f5a8c3dd33fcb9bf1d9b885746204701c7427d97
jpoley <jason.poley@gmail.com>
2022-07-23 05:56:03 -0400

    tailscale and vcluster

---
+++ tail.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt-get update
+sudo apt-get install tailscale
+sudo tailscale up
+
---
+++ vk8s.sh
@@ -0,0 +1,2 @@
+curl -s -L "https://github.com/loft-sh/vcluster/releases/latest" | sed -nE 's!.*"([^"]*vcluster-linux-amd64)".*!https://github.com\1!p' | xargs -n 1 curl -L -o vcluster && chmod +x vcluster;
+sudo mv vcluster /usr/local/bin;
